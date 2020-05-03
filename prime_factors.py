84053236064034945b724e6b6844d6ea6dd812bc
nicholaspoon03 <npoon2003@gmail.com>
2020-05-03 11:35:51 -0400

    upload

---
+++ prime_factors.py
@@ -0,0 +1,32 @@
+n = int(input('Enter an integer (2 or greater): '))
+number = n
+factor = 2
+factor_list = []
+prime = False
+
+if n >= 2:
+    for i in range(2, n+1):
+        if n == 2:
+            prime = True
+            break
+        elif n % i == 0:
+            break
+        else:
+            prime = True
+    
+    if not prime:
+        while factor <= n:
+            if number % factor == 0:
+                factor_list.append(factor)
+                number /= factor
+                factor = 2
+            else:
+                factor += 1
+    else:
+        factor_list.append(n)
+
+    print(f'The prime factors of {n} are:')
+    for i in factor_list:
+        print(i)
+else:
+    print('Please print a valid number.')
